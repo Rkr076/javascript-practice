e1c39296018f18590bd927cc9488d294010dce94
rkr09 <rahulgop.rock@gmail.com>
2024-12-20 21:50:54 +0530

    Created variable1 file & check typeOf datatype

---
+++ variable1.js
@@ -0,0 +1,8 @@
+"use strict";   //Treat all code as newest version
+
+let isLoggedIn = false;
+let artist = null;
+let city;
+// alert(3+3); // we are using node.js , not browser
+console.log(typeof artist);
+console.log(typeof city);
